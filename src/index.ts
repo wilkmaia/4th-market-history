ce36ff67ed14f4816f56c6a26c76d56df7e450ed
Wilk Maia <wilkmaia@gmail.com>
2022-06-22 12:26:23 -0300

    feat: add infinite runner

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { config as dotenvConfig } from 'dotenv'
-import type { Transaction as Neo4jTransaction } from 'neo4j-driver'
+import type {
+  Session,
+  Transaction as Neo4jTransaction,
+} from 'neo4j-driver'
 
 dotenvConfig()
 
@@ -11,31 +14,48 @@ const client = new Ragna4thDatabase({
   transactionsApiUrl: process.env.TRANSACTIONS_API_URL,
 })
 
-const run = async () => {
-  const transactions = await client.getLastTransactions()
+const mainLoop = async (session: Session) => {
+  while (true) {
+    console.debug('Starting new batch')
 
+    const transactions = await client.getLastTransactions()
+
+    try {
+      await session.writeTransaction((trx: Neo4jTransaction) =>
+        Promise.all(transactions.map(async t => {
+          const item = new db.DItem(t.item)
+          await item.sync(trx)
+
+          const transaction = new db.DTransaction(t)
+          await transaction.sync(trx)
+        }))
+      )
+    } catch (error: any) {
+      console.error({
+        message: 'Error on main loop',
+        errorMessage: error.message,
+        error,
+      })
+    }
+
+    if (transactions.length > 0) {
+      const max = transactions[transactions.length - 1].id
+      await client.setLastTransactionID(max)
+      console.debug(`Updating lastKnownTransactionID: ${max}`)
+    }
+
+    console.debug('Processed last transactions. Waiting for 3000ms...')
+    await new Promise(resolve => setTimeout(resolve, 3000))
+  }
+}
+
+const run = async () => {
   const session = db.driver.session()
 
-  try {
-    await session.writeTransaction((trx: Neo4jTransaction) =>
-      Promise.all(transactions.map(async t => {
-        const item = new db.DItem(t.item)
-        await item.sync(trx)
+  await mainLoop(session)
 
-        const transaction = new db.DTransaction(t)
-        await transaction.sync(trx)
-      }))
-    )
-  } catch (error: any) {
-    console.error({
-      message: 'Error on main loop',
-      errorMessage: error.message,
-      error,
-    })
-  } finally {
-    await session.close()
-    await db.close()
-  }
+  await session.close()
+  await db.close()
 }
 
 run()
